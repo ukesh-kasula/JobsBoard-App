308e20313c9857dbf6dd699d3ede3e8253e37952
Ukeshnani <ukeshnani10@gmail.com>
2025-07-15 19:10:01 +0530

    Add OpenRouter key injection at build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  openRouterApiKey: process.env['__OPENROUTER_API_KEY__']
+  openRouterApiKey: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  openRouterApiKey: ''
+  openRouterApiKey: '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'
 };
